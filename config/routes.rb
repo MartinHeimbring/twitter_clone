cf73530adc1983195645fed7fb8a2c5e88625785
Annabelle Martinel <annabellestephanie@gmail.com>
2015-01-08 00:51:21 +0100

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,8 @@ Rails.application.routes.draw do
   get 'users/new'
 
   get 'home', to: 'static_pages#home'
-
   get 'help', to: 'static_pages#help'
-
   get 'about', to: 'static_pages#about'
-
   get 'contact', to: 'static_pages#contact'
 
   get 'signup', to: 'users#new'
@@ -19,7 +16,9 @@ Rails.application.routes.draw do
 
   delete 'logout', to: 'sessions#destroy'
 
+
   resources :users
+  resources :account_activations, only: [:edit]
 
 
 
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,7 +21,7 @@ end
                                password: "password",
                                password_confirmation: "password"}
     end
-    assert_template 'users/show'
-    assert is_logged_in?
+    #assert_template 'users/show'
+    #assert is_logged_in?
   end
 end
